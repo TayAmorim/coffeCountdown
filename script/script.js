6fa2e4324977effa5b80134227049c6a1d51d36b
TayAmorim <amorim.tayanna@gmail.com>
2022-09-22 17:53:53 -0300

    terminando o countdown

---
+++ script/script.js
@@ -0,0 +1,34 @@
+const targetDate = new Date('December 31, 2022 00:00:00').getTime()
+
+const countdown = setInterval(function () {
+  const now = new Date().getTime()
+  const TimeDate = targetDate - now
+
+  let days = Math.floor(TimeDate / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((TimeDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  let minutes = Math.floor((TimeDate % (1000 * 60 * 60)) / (1000 * 60))
+  let seconds = Math.floor((TimeDate % (1000 * 60)) / 1000)
+
+  document.getElementById('countdown').innerHTML = `
+  <div id="countdown" class="countdown">
+          <ul>
+            <li>
+              <span>Dias</span>
+              <span class="icon">${days}</span>
+            </li>
+            <li>
+              <span>Horas</span>
+              <span class="icon">${hours}</span>
+            </li>
+            <li>
+              <span>Minutos</span>
+              <span class="icon">${minutes}</span>
+            </li>
+            <li>
+              <span>Segundos</span>
+              <span>${seconds}</span>
+            </li>
+          </ul>
+        </div>
+  `
+}, 1000)
